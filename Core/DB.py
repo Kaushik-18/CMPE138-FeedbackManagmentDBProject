39ab7bf2ea2e6c61f57460deb2499cb7f874aca9
gurnoors <gurnoor@gmail.com>
2016-11-18 03:49:59 -0800

    DB class to define query()

---
+++ Core/DB.py
@@ -0,0 +1,12 @@
+
+class DB:
+    __init__
+    #open a new connection
+
+    def query(self, table, paramsJson=None):
+        """Will execute the query and return rows as a list of (formatted) Strings.
+        Return empty list in case of No results"""
+        pass
+
+    def close(self):
+        """close connection"""
