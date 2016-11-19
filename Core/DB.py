b4acb800ae043b98a371c855c254190f861afd52
gurnoors <gurnoor@gmail.com>
2016-11-18 20:27:44 -0800

    query() should return a list of (respective Entity) objects (not list of strings)

--- Core/DB.py
+++ Core/DB.py
@@ -4,7 +4,7 @@ class DB:
     #open a new connection
 
     def query(self, table, paramsJson=None):
-        """Will execute the query and return rows as a list of (formatted) Strings.
+        """Will execute the query and return rows as a list of objects.
         Return empty list in case of No results"""
         pass
 
