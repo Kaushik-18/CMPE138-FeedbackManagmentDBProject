54c254d877ea38d3343033d00559a02b57474039
Madhur Khandelwal <madhurkhandelwal234@gmail.com>
2016-11-23 11:20:47 -0800

    Formate DB.py to follow PEP8

--- Core/DB.py
+++ Core/DB.py
@@ -3,7 +3,8 @@ import MySQLdb as mysql
 
 class DB:
     def __init__(self):
-        self.connection = mysql.connect(user="root", passwd="admin", db="cmpe138_project_team3_feedback")
+        self.connection = mysql.connect(user="root", passwd="admin",
+                                        db="cmpe138_project_team3_feedback")
 
     def close(self):
         """close connection"""
@@ -18,11 +19,13 @@ class DB:
         cursor = self.connection.cursor()
         if tablename == "product":
             cursor.execute(
-                "INSERT INTO product_feedback(ratings,customer_id,product_id,comments,franchise_id)VALUES (%s,%s,%s,%s,%s)",
+                "INSERT INTO product_feedback(ratings,customer_id,"
+                "product_id,comments,franchise_id)VALUES (%s,%s,%s,%s,%s)",
                 values)
         else:
             cursor.execute(
-                "INSERT INTO service_feedback(ratings,customer_id,service_id,comments,franchise_id)VALUES (%s,%s,%s,%s,%s)",
+                "INSERT INTO service_feedback(ratings,customer_id,service_id,"
+                "comments,franchise_id)VALUES (%s,%s,%s,%s,%s)",
                 values)
         self.connection.commit()
         self.close()
@@ -30,7 +33,8 @@ class DB:
     def update_record(self):
         pass
 
-    # TODO add query to check if product id is available for particular franchise record
+    # TODO add query to check if product id is available for particular
+    # franchise record
     def check_product_record(self, check_id, franchise_id):
         return True
 
@@ -44,8 +48,10 @@ class DB:
         return False
 
     # possible issue here ...we can add same feedback id in action item ...
-    # 1 solution is to make feedback id columns unique and null,but some suggest this is actually a bug in MySql
-    # 2 solution is to check if feedback id is already entered using above function
+    # 1 solution is to make feedback id columns unique and null,but some
+    #   suggest this is actually a bug in MySql
+    # 2 solution is to check if feedback id is already entered using above
+    #   function
     def insert_action_item(self, values, feedback_type):
         try:
             cursor = self.connection.cursor()
@@ -55,10 +61,11 @@ class DB:
                 item_type = "service_feedback_id"
 
             cursor.execute(
-                "INSERT INTO action_items(start_date,end_date,created_by,assigned_to,comments," + item_type + ")VALUES(%s,%s,%s,%s,%s,%s)"
-                , values)
+                "INSERT INTO action_items(start_date,end_date,created_by,"
+                "assigned_to,comments," + item_type +
+                ")VALUES(%s,%s,%s,%s,%s,%s)", values)
             self.connection.commit()
-        except mysql.Error, e:
+        except mysql.Error:
             print("Invalid ids entered")
         finally:
             self.close()
@@ -67,9 +74,10 @@ class DB:
 def update_action_item(self, values):
     try:
         cursor = self.connection.cursor()
-        cursor.execute("UPDATE action_items set action_status=%s where action_item_id=%s", values)
+        cursor.execute("UPDATE action_items set action_status=%s where "
+                       "action_item_id=%s", values)
         self.connection.commit()
-    except mysql.Error, e:
+    except mysql.Error:
         print("Invalid ids entered")
     finally:
         self.close()
